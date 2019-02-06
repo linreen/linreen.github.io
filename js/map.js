de959949b5764ba19ba08ee665a8d9ce6895a45b
linreen <lind.ren@mail.utoronto.ca>
2019-02-05 22:27:45 -0500

    modified map

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/dark-v9',//stylesheet location
     center: [0.1278,51.5074], // starting position
-    zoom: 9 // starting zoom
+    zoom: 7 // starting zoom
 });
 
 $(document).ready(function() {
@@ -29,23 +29,16 @@ function makeGeoJSON(csvData) {
         "type": "circle",
         "source": {'type': 'geojson',
                     'data': data},
-        "maxzoom": 10,
+        "maxzoom": 22,
         'paint': {
 // make circles larger as the user zooms from z12 to z22
 'circle-radius': {
-'base': 1.75,
-'stops': [[12, 2], [22, 180]]
+'base': 0.5,
+'stops': [[12, 1], [22, 4]]
 },
 // color circles by ethnicity, using a match expression
 // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-'circle-color': [
-'match',
-['get', 'Accident_Severity'],
-3, '#fbb03b',
-2, '#223b53',
-1, '#e55e5e',
-/* other */ '#ccc'
-]
+'circle-color': '#DC143C' 
 }
 });
 })})}
