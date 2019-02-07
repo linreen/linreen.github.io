a4be0f49d495160d2c9d422d92ff543e9fd50633
linreen <lind.ren@mail.utoronto.ca>
2019-02-07 16:38:05 -0500

    mod navbar

--- js/counter.js
+++ js/counter.js
@@ -1,37 +1,35 @@
 	function counter(){
-	  var strong1 = d3.select("#strong");
-	  var strong2 = d3.select("#strong2");
-	    
-	  var text = strong1.append("strong")
+	  var start = 0;
+	  var end1 = 6000;
+	  var end2 = 200;
+	  var text = d3.select("#strong")
+	    .append("strong")
 	    .attr("x", 50)
 	    .attr("y", 50)
-	    .text(1);
+	    .text(0);
 
-	  var text2 = strong2.append("strong")
+	  var text2 = d3.select("#strong2")
+	  .append("strong")
 	  .attr("x", 50)
 	  .attr("y", 50)
-	  .text(1);
+	  .text(0);
 	    
 	  text.transition()
-	    .tween("text", function starttrans() {
+	    .tween("text", function () {
 	       var selection = d3.select(this);    // selection of node being transitioned
-	       var start = d3.select(this).text(); // start value prior to transition
-	       var end = 6000;                     // specified end value
-	       var interpolator = d3.interpolateNumber(start,end); // d3 interpolator
+	       var interpolator = d3.interpolateNumber(start,end1); // d3 interpolator
 
-	       return function starttrans(t) { selection.text(Math.round(interpolator(t))); };  // return value
+	       return function (t) { selection.text(Math.round(interpolator(t))); };  // return value
 	       
 	    })
-	  .duration(2000);
+	  .duration(1000);
 	  text2.transition()
-	    .tween("text", function starttrans() {
-	       var selection = d3.select(this);    // selection of node being transitioned
-	       var start = d3.select(this).text(); // start value prior to transition
-	       var end = 200;                     // specified end value
-	       var interpolator = d3.interpolateNumber(start,end); // d3 interpolator
+	    .tween("text", function () {
+	       var selection2 = d3.select(this);    // selection of node being transitioned
+	       var interpolator = d3.interpolateNumber(start,end2); // d3 interpolator
 
-	       return function starttrans(t){selection.text(Math.round(interpolator(t))); };  // return value
+	       return function (t){selection2.text(Math.round(interpolator(t))); };  // return value
 	       
 	    })
-	  .duration(2000);
+	  .duration(1000);
 	}
