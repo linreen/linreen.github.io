b66b871cc8434d85f1c168a677ea631f5740e82c
linreen <lind.ren@mail.utoronto.ca>
2019-02-15 13:26:50 -0500

    reimplemented counter

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,9 @@
 	function counter(){
 	  var start = 0;
-	  var end1 = 6000;
-	  var end2 = 200;
+	  var end1 = 179715;
+	  var end2 = 241954;
+	  var end3 = 328066;
+
 	  var text = d3.select("#strong")
 	    .append("strong")
 	    .attr("x", 50)
@@ -13,23 +15,42 @@
 	  .attr("x", 50)
 	  .attr("y", 50)
 	  .text(0);
+
+  	  var text3 = d3.select("#strong3")
+	  .append("strong")
+	  .attr("x", 50)
+	  .attr("y", 50)
+	  .text(0);
 	    
 	  text.transition()
 	    .tween("text", function () {
 	       var selection = d3.select(this);    // selection of node being transitioned
-	       var interpolator = d3.interpolateNumber(start,end1); // d3 interpolator
+	       var interpolator1 = d3.interpolateNumber(100000,end1); // d3 interpolator
 
-	       return function (t) { selection.text(Math.round(interpolator(t))); };  // return value
+	       return function (t) { selection.text(Math.round(interpolator1(t))); };  // return value
 	       
 	    })
+	    .style("color", "black")
 	  .duration(1000);
 	  text2.transition()
 	    .tween("text", function () {
 	       var selection2 = d3.select(this);    // selection of node being transitioned
-	       var interpolator = d3.interpolateNumber(start,end2); // d3 interpolator
+	       var interpolator2 = d3.interpolateNumber(200000,end2); // d3 interpolator
 
-	       return function (t){selection2.text(Math.round(interpolator(t))); };  // return value
+	       return function (t){selection2.text(Math.round(interpolator2(t))); };  // return value
 	       
 	    })
-	  .duration(1000);
+	    .style("color", "black")
+	  .duration(2000);
+
+  	  text3.transition()
+	    .tween("text", function () {
+	       var selection3 = d3.select(this);    // selection of node being transitioned
+	       var interpolator3 = d3.interpolateNumber(300000,end3); // d3 interpolator
+
+	       return function (t){selection3.text(Math.round(interpolator3(t))); };  // return value
+	       
+	    })
+	    .style("color", "black")
+	  .duration(3000);
 	}
